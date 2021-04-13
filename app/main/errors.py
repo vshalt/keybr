a8482da37e0a74c7c507decd63cb4f934609b598
vishal <chocotacove@gmail.com>
2021-04-13 15:28:11 +0530

    fix: errorhandlers

--- app/main/errors.py
+++ app/main/errors.py
@@ -1,14 +1,14 @@
 from app.main import main_blueprint
 from flask import render_template
 
-@main_blueprint.app_errorhandler(400)
+@main_blueprint.app_errorhandler(404)
 def error_404(e):
-    return render_template('errors/404.html'), e
+    return render_template('errors/404.html'), 404
 
 @main_blueprint.app_errorhandler(403)
 def error_403(e):
-    return render_template('errors/403.html'), e
+    return render_template('errors/403.html'), 403
 
 @main_blueprint.app_errorhandler(500)
 def error_500(e):
-    return render_template('errors/500.html'), e
+    return render_template('errors/500.html'), 500
